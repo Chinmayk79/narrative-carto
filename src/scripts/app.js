241ef76a3c7d698400629599a9d3b05bc635a3d6
Chinmayk79 <122165522+Chinmayk79@users.noreply.github.com>
2024-02-09 11:16:00 +0530

    start

---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+import enterViewScrolly from '../scrolly/enterViewScrolly';
+/**
+ * Call the scrolly function with the ID of the section in HTML
+ * Can have more than one scrolly on a page
+ */
+enterViewScrolly('#doge-scrolly');
